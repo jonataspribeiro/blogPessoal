01deccdf27bafa48527b7e81685e4b2350258df9
jonataspribeiro <jonataspalumbo1993@gmail.com>
2021-08-30 17:03:51 -0300

    Consumo da API

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { UsuarioLogin } from '../entrar/model/UsuarioLogin';
 import { Observable } from 'rxjs';
 import { Usuario } from '../entrar/model/Usuario';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +22,14 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogpessoaljonatas.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+    ok = true
+    }
+    return ok
   }
 }
